4ad5cde7511d9da505ea0bc65a4a00fd59b7439f
Takashi Kokubun <takashikkbn@gmail.com>
2017-02-05 04:38:23 +0900

    Handle null boundary

--- src/main/java/com/github/k0kubun/lambda/gyazo/GyazoHandler.java
+++ src/main/java/com/github/k0kubun/lambda/gyazo/GyazoHandler.java
@@ -4,8 +4,21 @@ import com.amazonaws.services.lambda.runtime.Context;
 
 public class GyazoHandler {
     public String post(final Request request, final Context context) {
+        String boundary = getBoundary(request);
+        if (boundary == null) {
+            return "bad request";
+        }
+
+        return "Boundary:\n" +
+            boundary +
+            "\nBase64 body:\n" + request.getBody();
+    }
+
+    private String getBoundary(final Request request) {
+        if (request.getContentType() == null) {
+            return null;
+        }
         ContentType contentType = new ContentType(request.getContentType());
-        return "Boundary: " + contentType.getParameters().get("boundary") +
-            "\nBase64 body: " + request.getBody();
+        return contentType.getParameters().get("boundary");
     }
 }
