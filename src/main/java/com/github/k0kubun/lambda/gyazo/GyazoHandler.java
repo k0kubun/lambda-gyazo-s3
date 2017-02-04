73843b2c6d55f9d87be9d0b9e1c3afb31166118f
Takashi Kokubun <takashikkbn@gmail.com>
2017-02-05 02:43:00 +0900

    Add POJO Request class

--- src/main/java/com/github/k0kubun/lambda/gyazo/GyazoHandler.java
+++ src/main/java/com/github/k0kubun/lambda/gyazo/GyazoHandler.java
@@ -3,7 +3,7 @@ package com.github.k0kubun.lambda.gyazo;
 import com.amazonaws.services.lambda.runtime.Context;
 
 public class GyazoHandler {
-    public String post(final Context context) {
-        return "Hello world!";
+    public String post(final Request request, final Context context) {
+        return "Base64 body: " + request.getBody();
     }
 }
---
+++ src/main/java/com/github/k0kubun/lambda/gyazo/Request.java
@@ -0,0 +1,13 @@
+package com.github.k0kubun.lambda.gyazo;
+
+public class Request {
+    String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
